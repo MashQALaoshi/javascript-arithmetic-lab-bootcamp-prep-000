ee74499653c9e507e00dce748f610bd3df4d2223
mashur.hossain@gmail.com <mashur.hossain@gmail.com>
2018-05-17 04:34:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ function dec(n) {
 dec(5)
 
 function makeInt(n) {
-  return parseInt('n', 10)
+  return parseInt(n, 10)
 } 
 
 
 function preserveDecimal(n) {
-return parseFloat('n')  
+return parseFloat(n)  
 }
